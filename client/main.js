10cdf17cc2f420033594fb804b9583bd61c1012a
Kit Kei Li <kitli@ucdavis.edu>
2017-02-08 15:47:44 -0800

    initial commit

---
+++ client/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      React App #2
+    </div>
+  );
+};
+
+Meteor.startup( () => {
+  ReactDOM.render(<App />, document.querySelector('.container'));
+});
