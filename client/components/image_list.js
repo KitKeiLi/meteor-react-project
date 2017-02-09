99b2934c535913278a3b8e3e97a6d6ed85ea6389
Kit Kei Li <kitli@ucdavis.edu>
2017-02-08 18:20:00 -0800

    adds axios

---
+++ client/components/image_list.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  { title: 'Pen', link: 'https://dummyimage.com/600x400' },
+  { title: 'Pine Tree', link:'https://dummyimage.com/600x400' },
+  { title: 'Mug', link:'https://dummyimage.com/600x400' }
+];
+
+
+
+const ImageList = () => {
+  const RenderedImages = IMAGES.map( (image) => {
+    return <ImageDetail key={image.title} image={image} />
+  });
+
+
+  return (
+    <ul className="media-list list-group">
+      {RenderedImages}
+    </ul>
+  );
+};
+
+export default ImageList;
--- client/main.js
+++ client/main.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import ImageList from './components/image_list';
 
 const App = () => {
   return (
     <div>
-      React App #2
+      <ImageList />
     </div>
   );
 };
 
 Meteor.startup( () => {
-  ReactDOM.render(<App />, document.querySelector('.container'));
+    ReactDOM.render(<App />, document.querySelector('.container'));
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0.json')
+        .then(response => console.log(response));
 });
