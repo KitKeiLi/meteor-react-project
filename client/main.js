aa12e6cd925fc1aea37c0f80594a99dcc5579460
Kit Kei Li <kitli@ucdavis.edu>
2017-02-08 20:31:54 -0800

    adds ups/downs bar

--- client/main.js
+++ client/main.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
 
-const App = () => {
-  return (
-    <div>
-      <ImageList />
-    </div>
-  );
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { images: [] };
+    }
+
+
+    componentWillMount() {
+        axios.get('https://api.imgur.com/3/gallery/hot/viral/0.json')
+            .then(response => this.setState({ images: response.data.data }));
+    }
+
+    render() {
+        console.log(this.state.images)
+        return (
+            <div>
+                <ImageList images={this.state.images}/>
+            </div>
+        );
+    }
+}
 
 Meteor.startup( () => {
     ReactDOM.render(<App />, document.querySelector('.container'));
-    axios.get('https://api.imgur.com/3/gallery/hot/viral/0.json')
-        .then(response => console.log(response));
 });
